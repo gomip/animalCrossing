b35a4517272974705b59135fcd54c5ac1e845aff
김정훈 <junghoon15@rsquare.co.kr>
2020-06-10 17:38:52 +0900

    view swipe 생성

--- app/src/main/java/org/androidtown/animalcrossing/CardAdapter.kt
+++ app/src/main/java/org/androidtown/animalcrossing/CardAdapter.kt
@@ -7,28 +7,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.NonNull
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.*
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.androidtown.animalcrossing.MainFragment.fragmentIsland
 import org.androidtown.animalcrossing.MainFragment.fragmentSlave
 
 
-class CardAdapter : FragmentPagerAdapter {
+class CardAdapter(var fragmentList: MutableList<Fragment>, fa: FragmentActivity) : FragmentStateAdapter(fa) {
 
-    private val list : ArrayList<Fragment> = ArrayList()
-    constructor(fragmentManager: FragmentManager) : super(fragmentManager) {
-        list.add(fragmentIsland())
-        list.add(fragmentSlave())
+    override fun getItemCount(): Int {
+        return fragmentList.size
     }
 
-    override fun getItem(position: Int): Fragment {
-        return list.get(position)
-    }
-
-    override fun getCount() : Int {
-        return list.size
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
     }
 }
--- app/src/main/java/org/androidtown/animalcrossing/MainActivity.kt
+++ app/src/main/java/org/androidtown/animalcrossing/MainActivity.kt
@@ -1,31 +1,57 @@
 package org.androidtown.animalcrossing
 
+import android.content.Context
 import android.graphics.Color
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.DimenRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.appbar.MaterialToolbar
 import kotlinx.android.synthetic.main.activity_main.*
+import org.androidtown.animalcrossing.MainFragment.fragmentIsland
+import org.androidtown.animalcrossing.MainFragment.fragmentSlave
+import java.lang.Math.abs
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
 
     private lateinit var models : CardModel
-
-    var sliderDotspanel : LinearLayout?=null
-    private var dotscount = 0
-
     // 이름 저장 -> db로 변경
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = CardAdapter(supportFragmentManager)
-        viewPager.adapter = adapter
+        val pageList = mutableListOf<Fragment>(fragmentIsland(), fragmentSlave())
+
+        viewPager.adapter = CardAdapter(pageList, this)
+
+        // You need to retain one page on each side so that the next and previous items are visible
+        viewPager.offscreenPageLimit = 1
+
+        val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+        val currentItemHorizontalMarginPx = resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+        val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+        val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+            page.translationX = -pageTranslationX * position
+            // Next line scales the item's height. You can remove it if you don't want this effect
+            page.scaleY = 1 - (0.25f * abs(position))
+        }
+        viewPager.setPageTransformer(pageTransformer)
+
+        // The ItemDecoration gives the current (centered) item horizontal margin so that
+        // it doesn't occupy the whole screen width. Without it the items overlap
+        val itemDecoration = HorizontalMarginItemDecoration(this, R.dimen.viewpager_current_item_horizontal_margin)
+        viewPager.addItemDecoration(itemDecoration)
 
         // toolbar 제목 지정
         val toolbar : AppBarLayout = findViewById(R.id.toolbar)
@@ -33,5 +59,19 @@ class MainActivity : AppCompatActivity() {
         title.setText("도감")
     }
 
+    private class HorizontalMarginItemDecoration(context: Context, @DimenRes horizontalMarginInDp: Int) :
+        RecyclerView.ItemDecoration() {
+
+        private val horizontalMarginInPx: Int =
+            context.resources.getDimension(horizontalMarginInDp).toInt()
+
+        override fun getItemOffsets(
+            outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State
+        ) {
+            outRect.right = horizontalMarginInPx
+            outRect.left = horizontalMarginInPx
+        }
+
+    }
 
 }
